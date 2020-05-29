d02b4887b5f4096d7979d814acc384bb3a8b2b59
kamelbensalah <66136418+kamelbensalah@users.noreply.github.com>
2020-05-29 20:02:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//topnav
+function responsiveNavbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//slidshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
